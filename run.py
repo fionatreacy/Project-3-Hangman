acae5b70f2b878d433c493d55a94ce71c18d5e7a
fionatreacy <fionatreacy00@gmail.com>
2022-06-22 13:51:41 +0000

    add function to display incorrect guesses and attempts left

--- run.py
+++ run.py
@@ -2,3 +2,8 @@ import random
 from words.py import list_of_words
 
 def get_words():
+    """ 
+    Generates random word from word.py
+    """
+    word = random.word(list_of_words)
+    return word.upper()
