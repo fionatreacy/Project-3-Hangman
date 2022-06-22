cbdea5d166ba7c6b6444102f1de10f2fd7ca7d7b
fionatreacy <fionatreacy00@gmail.com>
2022-06-22 13:55:18 +0000

    add hangman visuals

--- run.py
+++ run.py
@@ -7,3 +7,88 @@ def get_words():
     """
     word = random.word(list_of_words)
     return word.upper()
+
+def play(word):
+    """
+    Displays game visuals including the hangman, number of attempts, incorrect guesses, and guessed words
+    """
+    completed_word = "_" * len(word)
+    guess = False
+    letters_guessed = []
+    words_guessed = []
+    attempts = 6
+
+
+def display_hangman(attempts):
+    stages = [  
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
